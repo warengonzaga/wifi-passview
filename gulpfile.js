7785e7d18c45a422ea5e0c27c4f083fcee94243a
Waren Gonzaga <emiren.warengonzaga.official@gmail.com>
2018-02-09 16:56:41 +0000

    Add help menu

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
 
 //Dependencies
 var fs = require('fs');
-var gulp = require('gulp');
+var gulp = require('gulp-help')(require('gulp'));
 var gutil = require('gulp-util');
 var clean = require('gulp-clean');
 
@@ -20,16 +20,16 @@ var path = {
     $core = JSON.parse(fs.readFileSync(path.core));
     
 //Core Tasks Don't Edit
-gulp.task('build', function(cb){
+gulp.task('build', 'To generate a custom builds inside the prod folder.', function(cb){
   fs.writeFile(path.prod+$config.config.filename+'_'+$config.config.version+'.bat', $core.codes01+$config.config.title+" "+$config.config.version+" "+$core.codes02+" "+$config.config.author+$core.codes03+$config.config.facebook+$core.codes04+$config.config.twitter+$core.codes05+$config.config.github+$core.codes06+$config.config.email+$core.codes07+$config.config.facebook+$core.codes08, cb);
 });
 
 
 //Clean Task
-gulp.task('clean', function() {
+gulp.task('clean', 'To clean the custom builds inside the prod folder.', function() {
    return gulp.src(path.prod+$config.config.filename+'_'+$config.config.version+'.bat', {read: false})
         .pipe(clean());
 });
 	
 //Default Tasks
-gulp.task('default', ['build']);
+gulp.task('default',false, ['build']);
